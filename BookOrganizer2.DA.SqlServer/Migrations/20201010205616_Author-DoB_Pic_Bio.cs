ec612803c09b03fa3c80a9109be129a3a78c73e4
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-10-10 23:57:27 +0300

    EF migration added.

---
+++ BookOrganizer2.DA.SqlServer/Migrations/20201010205616_Author-DoB_Pic_Bio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookOrganizer2.DA.SqlServer.Migrations
+{
+    public partial class AuthorDoB_Pic_Bio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Biography",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Authors",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MugshotPath",
+                table: "Authors",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Biography",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "MugshotPath",
+                table: "Authors");
+        }
+    }
+}
