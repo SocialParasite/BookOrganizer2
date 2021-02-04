d01cf0feb2f21a3dbc3c11f2225383a8f5c0aee3
Toni Jaakkola <toni.jaakkola@gmail.com>
2021-02-04 19:59:27 +0200

    Misc tweaks + fix
    Fixed book details view, so that authors that are selected as books authors do not show in available authors combobox

---
+++ BookOrganizer2.UI.BOThemes/Converters/EnumToTextConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BookOrganizer2.UI.BOThemes.Converters
+{
+    public class EnumToTextConverter
+        : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
